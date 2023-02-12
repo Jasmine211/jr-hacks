129e2d7915b442026d83ee9dbc8f50150149d102
Jasmine Perez <jasperez31@gmail.com>
2023-02-12 12:27:30 -0500

    edit debt page

--- routes/debt.js
+++ routes/debt.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const data = require("../data");
-const helpers = require("../helpers");
 
 router.get("/", async (req, res) => {
   return res.render("debt", {title: "Managing Your Debt"})
